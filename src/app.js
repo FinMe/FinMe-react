44ec5f53424471f9d757ff285cdd938e0f420169
Joshua Santiago <joshua@privco.com>
2018-08-26 12:58:45 -0400

    Added reactstrap which is a library which incorporates bootstrap to react easily. Added header navbar to project.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button } from 'reactstrap';
+
+// Load Components
+import Header from './components/Header.js';
 
 class FinMeApp extends React.Component {
    render() {
       return (
          <div>
             <p>FinMeApp</p>
+            <Header />
          </div>
       );
    }
